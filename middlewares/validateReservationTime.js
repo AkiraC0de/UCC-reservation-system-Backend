8818db81051a3edc468ec6fb46357ebafe8747ba
Akir4aa <ravenakira22@gmail.com>
2025-05-04 21:53:59 +0800

    Staring the validation of time middleware

--- middlewares/validateReservationTime.js
+++ middlewares/validateReservationTime.js
@@ -1,5 +1,9 @@
 const validateReservationTime = (req, res, next) => {
-    const { startTime, endTime } = req.body.startTime;
+    const { startTime, endTime } = req.body;
+
+    //check the format of time
+
+    const starHour = Number(startTime.slice(0,2));
 }
 
 module.exports = { validateReservationTime }
