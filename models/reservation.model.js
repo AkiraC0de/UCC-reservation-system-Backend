08e45554dfdbdb726e7df7b61fcfd6751639fa60
Akir4aa <ravenakira22@gmail.com>
2025-05-05 22:48:19 +0800

    Added reservation model without user scheme references

---
+++ models/reservation.model.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+const { validateTimeFormat } = require('../utils/validateTime')
+
+const reservationSchema = new mongoose.Schema({
+    date: {
+        type: Date,
+        required: true,
+    },
+    startTime: {
+        type: String,
+        required: true,
+        validate: {
+            validator: (value) => validateTimeFormat(value),
+            message: (props) => `${props.value} is not a valid time format. Expected format: HH:MM where MM can only be "00" or "30"`
+        }
+    },
+    endTime:  {
+        type: String,
+        required: true,
+        validate: {
+            validator: (value) => validateTimeFormat(value),
+            message: (props) => `${props.value} is not a valid time format. Expected format: HH:MM where MM can only be "00" or "30"`
+        }
+    },
+    status: {
+        enums: ['pending', 'confirmed', 'cancelled'],
+        default: 'pending'
+    },
+    purpose: String,
+    reservedBy: {
+        type: String, // REQUIRE AN UPDATE: String For now since user model isnt established yet
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        immutable: true,
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now
+      }
+})
